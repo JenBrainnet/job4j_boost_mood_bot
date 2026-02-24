e226e826adf86c0cfbc4de641209ab4fb9984d53
JenBrainnet <benebrak01@gmail.com>
2026-02-24 18:10:05 +0200

    Project Architecture "Mood Support Assistant Bot" [#505287]

---
+++ src/main/java/ru/job4j/AchievementService.java
@@ -0,0 +1,6 @@
+package ru.job4j;
+
+public class AchievementService {
+    private TelegramBotService telegramBotService;
+    private Repositories repositories;
+}
---
+++ src/main/java/ru/job4j/TelegramBotService.java
@@ -0,0 +1,6 @@
+package ru.job4j;
+
+public class TelegramBotService {
+    private Content content;
+    private BotCommandHandler botCommandHandler;
+}
