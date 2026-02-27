ea68a1218c19fe42b411b82c493726d0e4cfabb7
JenBrainnet <benebrak01@gmail.com>
2026-02-27 22:47:36 +0200

    @Profile | System Users [#505303]

--- src/main/java/ru/job4j/bmb/repository/UserRepository.java
+++ src/main/java/ru/job4j/bmb/repository/UserRepository.java
@@ -1,7 +1,14 @@
 package ru.job4j.bmb.repository;
 
 import org.springframework.stereotype.Repository;
+import ru.job4j.bmb.model.User;
+
+import java.util.List;
 
 @Repository
-public class UserRepository {
+public interface UserRepository {
+
+    List<User> findAll();
+    User findByClientId(Long clientId);
+
 }
