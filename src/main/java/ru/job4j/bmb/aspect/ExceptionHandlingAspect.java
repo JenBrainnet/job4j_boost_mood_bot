e2ed724e0bba26ea3c8303d4121b8af9ceadf990
JenBrainnet <benebrak01@gmail.com>
2026-03-20 03:02:08 +0200

    ExceptionHandlingAspect | AOP [#505310]

--- src/main/java/ru/job4j/bmb/aspect/ExceptionHandlingAspect.java
+++ src/main/java/ru/job4j/bmb/aspect/ExceptionHandlingAspect.java
@@ -1,7 +1,23 @@
 package ru.job4j.bmb.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+@Aspect
 @Component
 public class ExceptionHandlingAspect {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlingAspect.class);
+
+    @AfterThrowing(pointcut = "execution(* ru.job4j.bmb.service.*.*(..))", throwing = "ex")
+    public void handleException(JoinPoint joinPoint, Exception ex) {
+        LOGGER.error("An error occurred in method {} : {}",
+                joinPoint.getSignature().getName(),
+                ex.getMessage());
+    }
+
 }
