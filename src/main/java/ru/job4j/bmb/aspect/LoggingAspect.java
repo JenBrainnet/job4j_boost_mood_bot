51f2c27f0d1a2501048f8991ff9dde575decb2c1
JenBrainnet <benebrak01@gmail.com>
2026-03-20 02:47:11 +0200

    AOP concept [#505309]

--- src/main/java/ru/job4j/bmb/aspect/LoggingAspect.java
+++ src/main/java/ru/job4j/bmb/aspect/LoggingAspect.java
@@ -1,7 +1,24 @@
 package ru.job4j.bmb.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
+
+@Aspect
 @Component
 public class LoggingAspect {
+
+    @Pointcut("execution(* ru.job4j.bmb.service.*.*(..))")
+    private void serviceLayer() {}
+
+    @Before("serviceLayer()")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println("Вызов метода: " + joinPoint.getSignature().getName()
+                + ", args: " + Arrays.toString(joinPoint.getArgs()));
+    }
+
 }
--- src/main/java/ru/job4j/bmb/config/AppConfig.java
+++ src/main/java/ru/job4j/bmb/config/AppConfig.java
@@ -1,11 +1,14 @@
 package ru.job4j.bmb.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
-@Component
+@Configuration
+@EnableAspectJAutoProxy
 public class AppConfig {
 
     @Value("${app.name}")
     private String appName;
+
 }
